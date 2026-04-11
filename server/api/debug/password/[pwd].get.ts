9cb53cf6f6492b62ad7e4202a1975977e0bbd889
Nick Golebiewski <nick.golebiewski@gmail.com>
2026-04-10 21:14:22 -0400

    Deactivate hash debugger

--- server/api/debug/password/[pwd].get.ts
+++ server/api/debug/password/[pwd].get.ts
@@ -1,28 +1,31 @@
-import argon2 from 'argon2'
+// // TEMPORARILY DEACTIVATE!
 
-export default defineEventHandler(async (event) => {
-  // Grab the password from the URL parameter
-  const password = getRouterParam(event, 'pwd')
 
-  if (!password) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: 'Missing password in URL',
-    })
-  }
+// import argon2 from 'argon2'
 
-  try {
-    // Generate the hash using the server's environment
-    const hash = await argon2.hash(password)
+// export default defineEventHandler(async (event) => {
+//   // Grab the password from the URL parameter
+//   const password = getRouterParam(event, 'pwd')
+
+//   if (!password) {
+//     throw createError({
+//       statusCode: 400,
+//       statusMessage: 'Missing password in URL',
+//     })
+//   }
+
+//   try {
+//     // Generate the hash using the server's environment
+//     const hash = await argon2.hash(password)
     
-    return {
-      message: "Hash generated successfully",
-      hash: hash
-    }
-  } catch (error: any) {
-    return {
-      error: "Argon2 hashing failed",
-      details: error.message
-    }
-  }
-})
+//     return {
+//       message: "Hash generated successfully",
+//       hash: hash
+//     }
+//   } catch (error: any) {
+//     return {
+//       error: "Argon2 hashing failed",
+//       details: error.message
+//     }
+//   }
+// })
