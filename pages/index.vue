607e89ce3a9b24f38700f9eb197b42158e4a090b
Nick Golebiewski <nick.golebiewski@gmail.com>
2026-04-13 09:49:34 -0400

    Add alt tag for home hero

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@ const hero = "/images/ing.png" // put something in /public/images
   <div class="h-[calc(100vh-64px)] flex items-center justify-center">
     <img
       :src="hero"
+      alt="fullscreen artwork introducing Ryan Hill Studio, drawn text reads ING"
       class="max-h-full max-w-full object-contain"
     />
   </div>
